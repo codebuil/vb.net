ebc7d49dcb858d089875298758b2078ca206ca55
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-28 23:59:04 +0100

    user define function

---
+++ mins.vb
@@ -0,0 +1,16 @@
+Module Program
+
+    Sub Main()
+
+        'Displays Hello, World
+        Console.WriteLine("  \x1bc\x1b[42;30m\n {0}",Mins(20,40))
+
+    End Sub
+    
+    Function Mins(a as Integer,b as Integer)as Integer
+       dim aa as Integer=a
+       If aa>b Then aa=b
+       Return aa
+    End Function
+
+End Module
