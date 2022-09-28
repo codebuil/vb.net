ee33e3e7aaecae916fcf35a8fdf3a8c04c9fa998
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-28 23:47:26 +0100

    return string

---
+++ hello.vb
@@ -0,0 +1,14 @@
+Module Program
+
+    Sub Main()
+
+        'Displays Hello, World
+        Console.WriteLine("  \x1bc\x1b[42;30m\n {0}",Rets())
+
+    End Sub
+    
+    Function Rets()as String
+       Return "hello world....."
+    End Function
+
+End Module
