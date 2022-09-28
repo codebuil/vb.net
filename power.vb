09f203af1413f40e96bd5ca1f18148db8d8855ea
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-29 00:09:07 +0100

    use define  function

---
+++ power.vb
@@ -0,0 +1,25 @@
+Module Program
+
+    Sub Main()
+    Dim n As Integer
+
+        'Displays Hello, World
+        for n=0 to 16
+        Console.WriteLine("  \x1bc\x1b[42;30m\n {0}",power(2,n))
+        Next
+         
+    End Sub
+    
+    Function power(a as Integer,b as Integer)as Integer
+       Dim aa as Integer=a
+       Dim n As Integer
+       If b=0 Then Return 1
+       If b=1 Then Return a
+       For n=1 to b
+       aa=aa*a
+       Next
+      
+       Return aa
+    End Function
+
+End Module
