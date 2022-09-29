d21ee5837395c97d6f5986c6f7ad0505b669e870
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-29 20:21:01 +0100

    Create classs.vb

---
+++ classs.vb
@@ -0,0 +1,19 @@
+Module Program
+
+   Class starts
+         Sub on_starts()
+              Console.WriteLine(chr(27)+"[42;30m starts]")
+         End Sub
+   End Class
+
+
+    Sub Main()
+
+        dim s as new starts
+       s.on_starts()
+       
+
+    End Sub
+    
+
+End Module
