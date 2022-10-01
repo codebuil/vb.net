748af7137a4370245ee0c03766e9c5424acbe776
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-10-01 19:05:31 +0100

    mult 16 hex i64

---
+++ mult16.vb
@@ -0,0 +1,20 @@
+Module Program
+    Function mults(a As Long,b As Long)
+        Return a*b
+    End Function
+    Sub Main()
+        Dim n As Integer
+        Dim m As Long=1
+        
+        'Displays Hello, World
+        Console.WriteLine(chr(27)+"[42;30m]Hello, World!")
+        For n=0 to 64/4-2
+            Console.WriteLine("d{0}",m)
+            Console.WriteLine("h{0:x}",m)
+            m=mults(m,16)
+        Next
+
+    End Sub
+    
+
+End Module
